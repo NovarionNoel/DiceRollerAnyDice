828ba6bd863f856b6ccd3bca51fd08b7470787b3
NovarionNoel <100366305+NovarionNoel@users.noreply.github.com>
2022-02-24 11:06:52 -0500

    Add files via upload

---
+++ DiceRollerAnyDice/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static Random die = new Random();
+    static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("Let's roll some dice! How many sides do the dice you want to roll have?");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("How many dice do you want to roll?");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(DiceRoller(a,b) +  " is your total from those dice!");
+        Console.ReadKey();
+    }
+
+    static int DiceRoller(int x, int y)
+    {
+        int dsum = 0;
+        for (int i = 1; i <= y; i++)
+        {
+            int a = die.Next(1, (x+1));
+            dsum = dsum + a;
+        }
+        return dsum;
+    }
+
+
+}
